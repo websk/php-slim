0ff9b6ec33c3bc39e04d5f60f87435d5fb428b2b
s.kulkov <s.kulkov@vitrina.tv>
2026-05-20 10:04:48 +0300

    update redirect status

--- src/WebSK/Slim/Redirect.php
+++ src/WebSK/Slim/Redirect.php
@@ -14,7 +14,7 @@ class Redirect
      * @param int $status_code
      * @return ResponseInterface
      */
-    public static function redirect(ResponseInterface $response, $uri, int $status_code = StatusCodeInterface::STATUS_FOUND): ResponseInterface
+    public static function redirect(ResponseInterface $response, $uri, int $status_code = StatusCodeInterface::STATUS_TEMPORARY_REDIRECT): ResponseInterface
     {
         if ($uri instanceof UriInterface) {
             $url = $uri->getPath();
